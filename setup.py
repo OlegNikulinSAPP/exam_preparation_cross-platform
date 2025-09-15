999063eb092a532a52efd07227a60204ec023ea7
NikulinOV <oleg309@yandex.ru>
2025-09-15 16:59:29 +0300

    Мы начнем с создания нового workflow для GitHub Actions, который будет использовать Python-for-Android (p4a) вместо Buildozer

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ExamApp',
+    version='0.1',
+    description='Exam Preparation App',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=[''],
+    install_requires=['kivy'],
+    package_data={'': ['*.py', '*.kv', '*.png', '*.jpg', '*.atlas']},
+)
